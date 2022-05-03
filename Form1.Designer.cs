77aab95ed7e58cdf46f08dfe1c036f4a65a7f06e
Kutafilec <a.niedzielski94@wp.pl>
2022-05-03 21:29:49 +0200

    Added function StartTheQuiz() that sets variables to quiz  in random values

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -279,6 +279,7 @@
             this.startButton.TabIndex = 0;
             this.startButton.Text = "Start the quiz";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,94 @@ namespace Math_Quiz
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+        /// <summary>
+        /// Start the quiz by filling in all of the problem 
+        /// values and starting the timer. 
+        /// </summary>
+        /// 
+
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+        // These integer variables store the numbers 
+        // for the addition problem. 
+        int addend1;
+        int addend2;
+
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
+        public void StartTheQuiz()
+        {
+
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+        }
+            public Form1()
         {
             InitializeComponent();
         }
 
+
         private void label13_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
